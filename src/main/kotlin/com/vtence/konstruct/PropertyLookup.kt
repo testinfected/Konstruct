8ff2ac56ac35fc4e12ff47a16373660485cbca14
Vincent Tencé <vtence@gmail.com>
2022-10-03 23:40:48 -0400

    Add some sugar

--- src/main/kotlin/com/vtence/konstruct/PropertyLookup.kt
+++ src/main/kotlin/com/vtence/konstruct/PropertyLookup.kt
@@ -5,3 +5,6 @@ interface PropertyLookup<T> {
 
     fun <V> valueOf(property: Property<T, V>, defaultValue: V): V
 }
+
+
+fun <T, V> PropertyLookup<T>.valueOf(property: Property<T, V>, defaultValue: Provider<V>) = valueOf(property, defaultValue())
--- src/main/kotlin/com/vtence/konstruct/Providers.kt
+++ src/main/kotlin/com/vtence/konstruct/Providers.kt
@@ -33,3 +33,11 @@ class SortedSetOfValues<T : Comparable<T>>(private val values: Set<Provider<T>>)
         return values.map { it() }.toSortedSet(Comparator.naturalOrder())
     }
 }
+
+
+fun <T, R> Provider<T>.map(block: (T) -> R): Provider<R> = Provider { block(this()) }
+
+fun <T, R> Provider<T>.flatMap(block: (T) -> Provider<R>): Provider<R> = block(this())
+
+fun <T, R> Provider<T>.flatMap(block: (T) -> Maker<R>): Maker<R> = block(this())
+
